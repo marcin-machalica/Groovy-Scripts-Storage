81a0e6f2e79cb02f3c2ad7bd4779d07816565fa1
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-05-20 19:18:29 +0200

    Setup application.properties, added necessary migrations, added GroovyScript entity

---
+++ src/main/java/machalica/marcin/gss/groovyscripts/GroovyScript.java
@@ -0,0 +1,86 @@
+package machalica.marcin.gss.groovyscripts;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity(name = "groovy_script")
+public class GroovyScript {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@Size(min = 1, max = 100)
+	private String name;
+
+	@Size(max = 300)
+	private String body;
+
+	protected GroovyScript() {
+	}
+
+	public GroovyScript(@NotNull String name, String body) {
+		this.name = name;
+		this.body = body;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GroovyScript other = (GroovyScript) obj;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "GroovyScript [id=" + id + ", name=" + name + ", body=" + body + "]";
+	}
+
+}
