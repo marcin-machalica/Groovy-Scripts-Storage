a571053445d801f8c844b3040a36036ce06c516f
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-05-20 18:07:14 +0200

    Initial commit

---
+++ src/main/java/machalica/marcin/gss/GroovyScriptsStorageApplication.java
@@ -0,0 +1,13 @@
+package machalica.marcin.gss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyScriptsStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyScriptsStorageApplication.class, args);
+	}
+
+}
